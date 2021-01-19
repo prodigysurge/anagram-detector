a57c6225d68abf591b02fefba8b69ed8daa0f734
prodigysurge <boatinglife6@yahoo.com>
2021-01-19 14:38:39 -0500

    commit tree a0f62bedf7540756c66fc959c9904857c4c9c653

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+
+
+
+
+
+
+
+
+
+
+
+class Anagram
+    def initialize(words)
+        @words = words
+    end
+    
+    def match(words)
+        matching = letters(@words)
+        words.select do |word|
+            letters(word) == matching
+        end
+    end
+    def letters(selected)
+        selected.chars.sort
+    end
+end
+
+
+
